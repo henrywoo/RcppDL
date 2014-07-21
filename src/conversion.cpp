616629ad03523651758e56a47445a56634641630
thirdwing <kellyhavel1989@gmail.com>
2014-07-21 15:22:30 -0400

    deleted:    src/LogisticRegression.cpp 	deleted:    src/LogisticRegression.h 	modified:   src/conversion.cpp 	modified:   src/conversion.h 	deleted:    src/dA.cpp 	deleted:    src/dA.h 	deleted:    src/utils.h

--- src/conversion.cpp
+++ src/conversion.cpp
@@ -1,1 +1,37 @@
 #include "conversion.h"
+
+int ** RcppMatrixToC2DArray(Rcpp::IntegerMatrix& in)
+{
+  // get the vector dimension
+  int nrow = in.nrow();
+  int ncol = in.ncol();
+  
+  // allocate columns
+  int ** out = new int * [nrow];
+  for (int i=0; i<nrow; i++)
+  {
+    // allocate rows
+    out[i] = new int[ncol];
+    // get values
+    for (int j=0; j<ncol; j++)
+    {
+      out[i][j] = in(i,j);
+    }
+  }
+  // return C matrix
+  return out;
+}
+
+Rcpp::NumericMatrix CMatrixToRcppMatrix(int nbSample, int pbDimension, double** matrix)
+{
+  //NumericMatrix for matrix
+  Rcpp::NumericMatrix matrixOutput(nbSample,pbDimension);
+  for(int i=0;i<nbSample;i++)
+  {
+    for(int j=0;j<pbDimension;j++)
+    {
+      matrixOutput(i,j) = matrix[i][j];
+    }
+  }
+  return matrixOutput;
+}
--- src/conversion.h
+++ src/conversion.h
@@ -1,4 +1,10 @@
 #ifndef CONVERSION_H_
 #define CONVERSION_H_
 
+#include "Rcpp.h"
+
+int ** RcppMatrixToC2DArray(Rcpp::IntegerMatrix& in);
+
+Rcpp::NumericMatrix CMatrixToRcppMatrix(int nbSample, int pbDimension, double** matrix);
+
 #endif
